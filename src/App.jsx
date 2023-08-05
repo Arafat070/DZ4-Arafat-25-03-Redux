2e31dfe9a55666c09fb9a1b93dbef170385f9b43
Arafat070 <deud129@gmail.com>
2023-08-05 16:35:27 +0600

    Создал 2 компанента в одном в 1 где эти данные пользователя заполняются а во втором эти данные показываются

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './components/Form';
+import ShowForm from './components/ShowForm';
+
+const App = () => {
+    return (
+        <>
+            <Form />
+            <ShowForm />
+        </>
+    );
+};
+
+export default App;
